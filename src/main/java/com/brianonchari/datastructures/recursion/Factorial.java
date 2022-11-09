18eca1d20baf46cc2c89b22e915e7d0a3f041cef
Brianonchari <oncharibrian@gmail.com>
2022-11-09 11:22:16 +0300

    check for integers less than 1 in recursion

--- src/main/java/com/brianonchari/datastructures/recursion/Factorial.java
+++ src/main/java/com/brianonchari/datastructures/recursion/Factorial.java
@@ -1,5 +1,13 @@
 package com.brianonchari.datastructures.recursion;
 
+/**
+ * ============================
+ * STEPS TO WRITING A RECURSION FUNCTION
+ * 1. Identify the recursive case - the flow
+ * 2. Create a base case - the criterion
+ * 3. Unintentional case - the constraint  i.e Make sure the function stops for every possible argument of n
+ *
+ * */
 public class Factorial {
   public static void main(String[] args) {
     Factorial factorial = new Factorial();
@@ -8,6 +16,10 @@ public class Factorial {
   }
 
   public int factorial(int n) {
+    if (n<1) {
+      return -1;
+    }
+
     if (n == 1 || n == 0) {
       return 1;
     } else {
