c785bdab9abff2135dcb9a814e4880cfcf733d19
Brianonchari <oncharibrian@gmail.com>
2023-01-31 12:58:37 +0300

    format code

--- src/main/java/com/brianonchari/datastructures/recursion/Fibonacci.java
+++ src/main/java/com/brianonchari/datastructures/recursion/Fibonacci.java
@@ -1,34 +1,33 @@
 package com.brianonchari.datastructures.recursion;
 
 public class Fibonacci {
-    public static void main(String [] args){
-        int n = 10;
-        Fibonacci fibonacci = new Fibonacci();
-        fibonacci.fibonacci(n);
-        var rec = fibonacci.fibonacci2(n);
-        System.out.println(rec);
+  public static void main(String[] args) {
+    int n = 10;
+    Fibonacci fibonacci = new Fibonacci();
+    fibonacci.fibonacci(n);
+    var rec = fibonacci.fibonacci2(n);
+    System.out.println(rec);
+  }
+
+  public void fibonacci(int n) {
+    int num1 = 0;
+    int num2 = 1;
+    int counter = 0;
+
+    while (counter < n) {
+      System.out.println(num1 + "");
+      int num3 = num1 + num2;
+      num1 = num2;
+      num2 = num3;
+      counter = counter + 1;
+    }
+  }
+
+  public int fibonacci2(int n) {
+    if (n == 1 || n == 0) {
+      return n;
     }
 
-    public  void fibonacci(int n){
-        int num1 = 0;
-        int num2 = 1;
-        int counter = 0;
-
-        while (counter < n){
-            System.out.println(num1 + "");
-            int num3 = num1 + num2;
-            num1 = num2;
-            num2 = num3;
-            counter = counter + 1;
-
-        }
-    }
-
-    public int fibonacci2(int n){
-        if(n==1 || n ==0){
-            return n;
-        }
-
-        return fibonacci2(n-1) + fibonacci2(n-2);
-    }
+    return fibonacci2(n - 1) + fibonacci2(n - 2);
+  }
 }
