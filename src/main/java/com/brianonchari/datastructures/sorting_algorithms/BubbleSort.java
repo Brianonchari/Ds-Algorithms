2585239f27896cc13b7dfb4173a20a1276e46b1b
Brianonchari <oncharibrian@gmail.com>
2023-04-11 17:29:43 +0300

    Bubble Sort

--- src/main/java/com/brianonchari/datastructures/sorting_algorithms/BubbleSort.java
+++ src/main/java/com/brianonchari/datastructures/sorting_algorithms/BubbleSort.java
@@ -2,29 +2,29 @@ package com.brianonchari.datastructures.sorting_algorithms;
 
 public class BubbleSort {
 
-    public static void main(String[] args){
-        int arr[] = {9,1,3,4,5,78};
+    public static void main(String[] args) {
+        int arr[] = {4, 2, 1, 5, 3};
         BubbleSort bubbleSort = new BubbleSort();
         bubbleSort(arr);
         bubbleSort.printArray(arr);
 
     }
 
-    public static void bubbleSort(int[] arr){
+    public static void bubbleSort(int[] arr) {
         int temp = 0;
-        for(int i = 0;i< arr.length-1;i++){
-            if(arr[i]>arr[i+1]){
-                temp = arr[i];
-                arr[i] = arr[i+1];
-                arr[i+1] = temp;
-
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
             }
         }
     }
 
     /* Prints the array */
-    void printArray(int arr[])
-    {
+    public void printArray(int[] arr) {
         int n = arr.length;
         for (int i = 0; i < n; ++i)
             System.out.print(arr[i] + " ");
